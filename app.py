1538e9feb375830ad7ff94469ceb7c91eb102b81
felipeflourwears <A01274880@tec.mx>
2024-01-24 11:06:09 -0600

    Update PDF

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, abort
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, abort, make_response
 
 #Instances Models
 from models.ModelToken import ModelToken
@@ -10,6 +10,9 @@ import time
 import os
 import string
 import random
+import pdfkit
+import base64
+import datetime
 
 # Variables globales para almacenar el token y su tiempo de expiración
 token_info = {
@@ -25,7 +28,7 @@ load_dotenv()
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'mp4'}
 
-token = '60bb5b612f4f912207dcae25c4ded973'
+token = 'bf022becb3153f7e400ded1995ab071f'
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -157,6 +160,142 @@ def submit_form_simulate_api():
         ModelActions.upload_media_player_simulate(token, selected_player_id, temperature_variant)
     return redirect(url_for('index'))
 
+
+@app.route('/download_report')
+def download_report():
+     # Obtener la fecha y hora actual
+    now = datetime.datetime.now()
+    date = now.strftime("%d/%m/%Y")
+
+    # Formatear la fecha y hora actual según tu especificación
+    formatted_date = now.strftime("%d%m%Y%H%M%S") + '-' + str(random.randint(100, 999))
+    # Ruta del directorio actual del script
+    ruta_script = os.path.dirname(os.path.abspath(__file__))
+    # Ruta de la imagen
+    ruta_imagen = os.path.join(ruta_script, 'static','img', 'black.jpg')
+    print("RUTA Imagen: ", ruta_imagen)
+    # Leer la imagen en formato base64
+    with open(ruta_imagen, 'rb') as img_file:
+        img_base64 = base64.b64encode(img_file.read()).decode('utf-8')
+
+
+    get_players = ModelActions.getPlayerList(token)
+    #print(get_players)
+    # Ruta al ejecutable wkhtmltopdf en tu sistema
+    ruta_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+    config = pdfkit.configuration(wkhtmltopdf=ruta_wkhtmltopdf)
+    try:
+        contenido_pdf = f"""
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <meta charset="UTF-8">
+                <title>Players PopAtelier</title>
+                <style>
+                    header {{
+                        text-align: center;
+                    }}
+                    img {{
+                        max-width: 500px;
+                    }}
+                    .info {{
+                        text-align: left;
+                        font-size: 20px;
+                        margin-top: 10px;
+                        margin-left: 10px;
+                    }}
+                    .table-container {{
+                        margin-top: 20px;
+                        margin-left: 10px;
+                        width: 98%;
+                    }}
+                    .headers-doc{{
+                        margin-top: 50px;
+                    }}
+                    table {{
+                        border-collapse: collapse;
+                        width: 100%;
+                        margin-top: 10px;
+                    }}
+                    th, td {{
+                        border: 1px solid black;
+                        padding: 8px;
+                        text-align: left;
+                        vertical-align: top; /* Alineación vertical */
+                        line-height: 1.4; /* Ajuste del espaciado vertical */
+                    }}
+                    th {{
+                        background-color: black;
+                        color: white;
+                        text-align: center;
+                    }}
+                    hr {{
+                        margin-top: 50px;
+                        border: none;
+                        border-top: 2px solid black;
+                        width: 100%;
+                    }}
+                </style>
+            </head>
+            <body>
+                <header>
+                    <img src="data:image/jpeg;base64,{img_base64}" alt="Logo"> <!-- Imagen incrustada en formato base64 -->
+                    <div class="info">
+                        <p><strong>POP ATELIER LLC</strong></p>
+                        <p><strong>Date: </strong>{date}</p>
+                    </div>
+                </header>
+        """
+        # Construir el contenido del PDF usando los datos recibidos
+        contenido_pdf += '<h1>Player information</h1>'
+       # Crear una tabla HTML con estilo
+        contenido_pdf += '<table border="1" style="border-collapse: collapse; width: 100%; text-align: center;">'
+        contenido_pdf += '<tr>'
+        contenido_pdf += '<th style="padding: 10px;">Name</th>'
+        #contenido_pdf += '<th style="padding: 10px;">Player ID</th>'
+        contenido_pdf += '<th style="padding: 10px;">Serial Number</th>'
+        contenido_pdf += '<th style="padding: 10px;">IP Address</th>'
+        contenido_pdf += '<th style="padding: 10px;">Product Name</th>'
+        contenido_pdf += '<th style="padding: 10px;">Online Status</th>'
+        contenido_pdf += '<th style="padding: 10px;">Last Online Time</th>'
+        contenido_pdf += '</tr>'
+
+        for player in get_players:
+            # Obtener el estado en línea y establecer el color del cuadrado con bordes redondeados
+            status_color = 'green' if player["onlineStatus"] == 1 else 'red'
+            
+            contenido_pdf += '<tr>'
+            contenido_pdf += f'<td style="padding: 10px;">{player["name"]}</td>'
+            #contenido_pdf += f'<td style="padding: 10px;">{player["playerId"]}</td>'
+            contenido_pdf += f'<td style="padding: 10px;">{player["sn"]}</td>'
+            contenido_pdf += f'<td style="padding: 10px;">{player["ip"]}</td>'
+            contenido_pdf += f'<td style="padding: 10px;">{player["productName"]}</td>'
+            contenido_pdf += f'<td style="padding: 5px; text-align: center; color: white; background-color: {status_color}; border-radius: 0px;">&nbsp;</td>'
+            contenido_pdf += f'<td style="padding: 10px;">{player["lastOnlineTime"]}</td>'
+            contenido_pdf += '</tr>'
+
+        contenido_pdf += '</table>'
+
+        contenido_pdf += '</body></html>'
+
+        pdfkit_options = {
+            'page-size': 'A4',
+            'encoding': 'UTF-8',  # Especificar la codificación UTF-8
+            # Otras opciones de configuración si las necesitas
+        }
+
+        # Generar el PDF
+        pdf = pdfkit.from_string(contenido_pdf, False, configuration=config, options=pdfkit_options)
+
+        # Crear la respuesta con el PDF como descarga
+        response = make_response(pdf)
+        response.headers['Content-Type'] = 'application/pdf'
+        response.headers['Content-Disposition'] = 'attachment; filename=report-players.pdf'
+
+        return response
+    except Exception as e:
+        print("Error:", e)  # Imprime el error en la consola del servidor
+        return "Error al generar el PDF", 500
 
 @app.route('/')
 def index():
